e3b25413fe757d49e62376c040605bbe61a4539e
Max F. Albrecht <1@178.is>
2013-07-19 13:46:39 +0200

    mill-cli: more annotations

--- mill-cli.js
+++ mill-cli.js
@@ -27,22 +27,58 @@
 // 
 //  ---
 
-// First, we need to **require()** some modules from:
+// # SETUP
 // 
-// - the `node.js` core
-//     * *path* for working with paths
+// *The setup process is idiomatic for every nodejs file in this project, so it is only verbosely annotated here*
+// 
+// Since we start from nothing, we need to require some modules.
+// 
+// Some of them come from the `node.js` core, they can be required without installing them. 
+// 
+// - we need *path* for working with paths.
 var path = require('path'),
-// - the npm registry (these need to be declared as dependencies in `package.json`, so they will be **install**ed by `npm`)
-//     * the *flatiron* anti-framework 
+
+// <small> \smallsize
+// *A Note about external modules:* After `npm install --save something` in the app dir, they are installed in the sub-folder 'node_modules', thus will be found *automagically*. The `--save` falgs also instructs `npm` to write this 'dependency' to the `package.json` file.
+// </small> /normalsize
+// 
+// Now, we `require()` our external modules.
+// - the *flatiron* anti-framework 
     flatiron = require('flatiron');
-// - (some modules will be built in) 
 
-// We are a flatiron app!!!11!! So proud.
+//  
+// # {APP}
+// 
+// We start out with an 'app' object, which we get from the `flatiron` module.
+
+
 var app = mill = module.exports = flatiron.app;
-
+// This is a "injection container", provided by the ['broadway' module](https://github.com/flatiron/broadway).
+// This means plugins can modify the app object directly.
+// Where do these plugins come from?
+// - some are built-in and already activated by flatiron: (`log`, `conf`, `router`)
+// - some are built-in, but need to be activated (`http`, `cli`, …)
+// - Your own plugins need to activated as well
 // From flatiron, we get a bunch of stuff (but not too much)
-// - configuration management by nconf // TODO: move to module
-app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
+
+// 
+// # Configuration
+// 
+// Before doing anything with our `app`, we load the configuration. app.config is another built-in part of flatiron: the [`nconf` module](https://github.com/flatiron/nconf)
+// In short, it allows us to use 3 pre-configured sources where our configuration could come from:
+// - a `JSON` file, ie. `./config.json` -> `{ "foo": 1337 }`
+// - the environment variables of the process, ie. `$ export foo=1337 && mill`
+// - command line flags, ie. `mill --foo 1337`
+// 
+// The order in which we load the config specifies the order in which they are used! First is more important than second, etc.
+// You can think of the config file as default settings, the environment variables as per-system setting and command line arguments as per-run settings, thus they are used in that order.
+//
+
+app.config.argv(); // conf source: arguments is most important
+app.config.env();  // then env vars
+app.config.file({ file: path.join(__dirname, 'config', 'config.json') }); // lastly, our config.json file
+// FIXME: set the dir manually 
+app.config.set('cwd', process.cwd);
   
 // - also use the "cli" plugin (enables lazy-loading commands and color output)
 app.use(flatiron.plugins.cli, {
